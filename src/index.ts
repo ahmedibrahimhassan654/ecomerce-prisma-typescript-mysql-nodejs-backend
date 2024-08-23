5a757d20e53a1dc069e4024f841479ed9fb8162c
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2024-08-23 16:28:18 +0300

    Initialising Prisma in Express app

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
+import { PrismaClient } from "@prisma/client";
 import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
+const prisma = new PrismaClient();
 
 app.get("/", async (req: Request, res: Response) => {
   res.send("working ");
 });
 
 app.listen(3000, () => {
-  console.log("app listining ");
+  console.log("Server is running on port 3000");
 });
